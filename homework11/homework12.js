edd41273cdb6c0fe90df391521c96dc67c98f278
gunslinger1919 <shawn.roe@umontana.edu>
2023-04-18 16:53:23 -0600

    homework 12

--- homework11/homework12.js
+++ homework11/homework12.js
@@ -1,6 +1,4 @@
-
 var x = 100;
-
 var y = 100;
 var diameter = 25;
 
@@ -24,139 +22,132 @@ var mousey = 10;
 function setup() {
   createCanvas(800, 600);
 
-  shape1xSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-  shape1ySpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-
-
+  shape1xSpeed = Math.floor(Math.random() * Math.floor(Math.random() * 5) + 1);
+  shape1ySpeed = Math.floor(Math.random() * Math.floor(Math.random() * 5) + 1);
 }
 function draw() {
   background(0);
   fill(12, 30, 115);
 
+  //call create borders function
+  createBorders(10);
+
+  //call moving character function
+  createCharacter(x, y, diameter);
+
+  // call character movement
+  characterMovement();
+
+  //mouseClick
+
+  mouseClicked();
+
+// You win
+
+ youWin();
+
+
+  //character limits
+
+  createCharacterLimits();
+
+  // create enemy
+  createEnemy();
+
+
+
+  
+
+  
+  
+
+
+
+
+ 
+
+  circle(mousex, mousey, 10);
+}
+
+function createBorders(thickness) {
   //top boreder
-  rect(0, 0, width, 10);
+  rect(0, 0, width, thickness);
 
   //left border
-  rect(0, 10, 10, height);
-
+  rect(0, 0, thickness, height);
   //bottom border
-  rect(0, height - 10, width, 10);
+  rect(0, height - thickness, width, thickness);
 
   // right upper border
 
-  rect(width - 10, 0, 10, height - 50);
+  rect(width - thickness, 0, thickness, height - 50);
+}
 
-  // exit message
-  textSize(20);
-  text("exit", width - 50, height - 50);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  circle(x, y, diameter)
-
+function createCharacter(x, y, diameter) {
+  circle(x, y, diameter);
+}
+function characterMovement() {
   if (keyIsDown(s)) {
-
     y += 10;
-  }
-  else if (keyIsDown(w)) {
+  } else if (keyIsDown(w)) {
     y -= 10;
-
   }
 
   if (keyIsDown(d)) {
     x += 10;
-
-  }
-
-  else if (keyIsDown(a)) {
-
+  } else if (keyIsDown(a)) {
     x -= 10;
   }
+}
 
-  if (x > width)
-  {
+function mouseClicked() {
+  circle(mousex, mousey, 10);
+  mousex = mouseX;
+  mousey = mouseY;
+}
+function createCharacterLimits() {
+  if (x > width) {
     x = 0;
   }
 
-  if (x< 0)
-  {
+  if (x < 0) {
     x = width;
   }
 
-  if (y > height)
-  {
+  if (y > height) {
     y = 0;
   }
-  if ( y < 0)
-{
-  y =  height;
+  if (y < 0) {
+    y = height;
+  }
+
+  
+
+
 }
-
-// check to see if you won
-
-if ( x > width && x > width-50 )
-
-{
-
-  fill(10);
-  stroke(10);
-  textSize(50);
-  text("Winner!" ,width/2-50, height/2-50);
-}
-  // enemy
-
-  fill(10,100,20);
+function createEnemy() {
+  fill(30, 100, 20);
   //make shape
-  circle(shape1x,shape1y,shape1diameter);
+  circle(shape1x, shape1y, shape1diameter);
 
   shape1x += shape1xSpeed;
   shape1y += shape1ySpeed;
 
-  if (shape1y >= 600)
-  {
+  if (shape1y >= 600) {
     shape1y = 10;
+  } else if (shape1x >= 800) {
+    shape1x = 10;
   }
-  else if (shape1x >= 800)
-
-{
-  shape1x = 10;
-}
-
-
-circle(mousex,mousey, 10, 10,);
-
-
 
 }
-function mouseClicked() {
-  mousex = mouseX;
-  mousey = mouseY;
+
+function youWin()
+{console.log(x); 
+  if (x > width && y > height - 50) {
+    fill(255);
+    stroke(10);
+    textSize(50);
+    text("Winner!", width / 2 - 50, height / 2 - 50);
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
